07d66cd25cfa5cdbb25b405a1609f3e0c3dea842
matthewjung04 <matthew.jung95@gmail.com>
2024-12-31 23:56:59 -0500

    initial commit for bandsite v2 repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# BandSite Project
+
+## API
+[API Documentation](https://unit-2-project-api-25c1595833b2.herokuapp.com/)
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
