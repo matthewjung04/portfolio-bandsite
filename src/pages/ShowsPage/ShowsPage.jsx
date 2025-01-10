363b91393e9c7ead639db1ab6551cb57a1cbc58c
matthewjung04 <matthew.jung95@gmail.com>
2025-01-09 23:58:26 -0500

    updated show table but has bug in mobile site

--- src/pages/ShowsPage/ShowsPage.jsx
+++ src/pages/ShowsPage/ShowsPage.jsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react";
-
+import { BandSiteApi } from "../../utils/utils";
 import './ShowsPage.scss'
 
 function ShowsPage() {
+  const [shows, setShows] = useState(null);
 
-  
+  useEffect(() => {
+    const fetchShows = async () => {
+      let BandSiteGet = new BandSiteApi();
+      let showDates = await BandSiteGet.getShows();
+      setShows(showDates);
+    }
+    fetchShows();
+  },[])
+  console.log(shows)
   return (
     <section>
       <article className="shows-hero">
@@ -13,7 +22,8 @@ function ShowsPage() {
             <p className="shows-hero__content__desc">Beautiful Beasts Album</p>
             <h1 className="shows-hero__content__title"> Stripes of Yellow x For The Stings</h1>
           </div>
-          <div className="shows-hero__content__music">
+
+          {/* <div className="shows-hero__content__music">
             <iframe
               width="100%" height="166"
               scrolling="no" frameBorder="no" allow="autoplay"
@@ -24,9 +34,42 @@ function ShowsPage() {
               ·
               <a href="https://soundcloud.com/rose-music-official/apt" title="APT." target="_blank" className="music-player__link-two">APT.</a>
             </div>
-          </div>
+          </div> */}
         </div>
       </article>
+
+      {/* <article className="shows">
+        <h1 className="shows__title">Shows</h1>
+        <table className="shows__table">
+          <tbody>
+            <tr>
+              <th id="header1" className="head">DATE</th>
+              <th id="header2">VENUE</th>
+              <th id="header3">LOCATION</th>
+            </tr>
+            {
+              shows ? (
+                shows.map((show) => (
+                  <tr key={show.id} id={show.id} className='table-cursor'>
+                    <th className='shows__table__header--first'>DATE</th>
+                    <td className='shows__table__data--first'>{new Date(show.date).toDateString()}</td>
+                    
+                    <th className='shows__table__header'>VENUE</th>
+                    <td className='shows__table__data'>{show.place}</td>
+
+                    <th className='shows__table__header'>LOCATION</th>
+                    <td className='shows__table__data'>{show.location}</td>
+
+                    <td className='shows__table__data--last'>
+                      <button type='button' className='shows__table__data__button'>BUY TICKETS</button>
+                    </td>
+                  </tr>
+                ))
+              ) : null
+            }
+          </tbody>
+        </table>
+      </article> */}
     </section>
   )
 }
